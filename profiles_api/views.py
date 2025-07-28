a642727b5756cfc2e9075bac44c6dfbe67548a8d
Sunny <sundet.kz@gmail.com>
2025-07-29 04:42:02 +0500

    Add test Hello APIView with corresponding urls

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+        an_apiview = [
+            'Discipline is the key to success',
+            'Hardwork is a part of the path',
+            'Luck is sometimes more important',
+            ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
